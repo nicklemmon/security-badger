6d9ed2d7dbdf3b01262965ed2895f5e968286a91
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-12-04 10:32:57 -0500

    main - Add ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['standard', 'plugin:node/recommended', 'plugin:prettier/recommended'],
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'module',
+  },
+  ignorePatterns: ['/node_modules/**'],
+  rules: {
+    'space-before-function-parent': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    camelcase: 'off',
+  },
+}
