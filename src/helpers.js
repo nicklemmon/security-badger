c26e88933aeac910330f55cfbdb4863619e77167
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-12-04 14:25:52 -0500

    main - Remove destructure

--- src/helpers.js
+++ src/helpers.js
@@ -1,23 +1,23 @@
 // TODO: can't this be done within the GraphQL query itself?
 function formatVulnerabilityAlerts(data) {
   console.log('data', data)
-  const {
-    repository: {
-      vulnerabilityAlerts: { edges },
-    },
-  } = data
+  // const {
+  //   repository: {
+  //     vulnerabilityAlerts: { edges },
+  //   },
+  // } = data
 
-  return edges.map(edge => {
-    const advisory = edge.node.securityAdvisory
-    const { vulnerabilities, ...rest } = advisory
-    const firstVulnerabilityNode = vulnerabilities.edges[0].node
-    const vulnerableVersionRange = firstVulnerabilityNode.vulnerableVersionRange
+  // return edges.map(edge => {
+  //   const advisory = edge.node.securityAdvisory
+  //   const { vulnerabilities, ...rest } = advisory
+  //   const firstVulnerabilityNode = vulnerabilities.edges[0].node
+  //   const vulnerableVersionRange = firstVulnerabilityNode.vulnerableVersionRange
 
-    return {
-      ...rest,
-      versionRange: vulnerableVersionRange,
-    }
-  })
+  //   return {
+  //     ...rest,
+  //     versionRange: vulnerableVersionRange,
+  //   }
+  // })
 }
 
 module.exports = {
