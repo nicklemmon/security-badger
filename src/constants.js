36f747beb182cf06d37a18aba7e152cfb8dd638e
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-12-07 14:49:58 -0500

    main - Remove logging

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,8 @@
 const GITHUB_API_URL = 'https://api.github.com/graphql'
-const GITHUB_REPOSITORY = process.env.GITHUB_REPO
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL
 const SLACK_CHANNEL = process.env.SLACK_CHANNEL || process.env.INPUT_SLACKCHANNEL
-
-console.log('GITHUB_REPOSITORY', GITHUB_REPOSITORY)
-console.log('GITHUB_TOKEN', GITHUB_TOKEN)
-console.log('SLACK_WEBHOOK_URL', SLACK_WEBHOOK_URL)
 
 module.exports = {
   GITHUB_API_URL,
