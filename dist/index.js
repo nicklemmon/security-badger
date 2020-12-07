d2d3c3082911aada82a806f522495e06a9adce08
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-12-07 14:29:31 -0500

    main - Add console logs

--- dist/index.js
+++ dist/index.js
@@ -2670,6 +2670,10 @@ module.exports = /******/ (function (modules, runtime) {
       const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL
       const SLACK_CHANNEL = process.env.SLACK_CHANNEL || process.env.INPUT_SLACKCHANNEL
 
+      console.log('GITHUB_REPOSITORY', GITHUB_REPOSITORY)
+      console.log('GITHUB_TOKEN', GITHUB_TOKEN)
+      console.log('SLACK_WEBHOOK_URL', SLACK_WEBHOOK_URL)
+
       module.exports = {
         GITHUB_API_URL,
         GITHUB_REPOSITORY,
