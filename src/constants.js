83136fb54dda3f023d2052fe42cd92b8ff8ccb51
Nick Lemmon <nick.lemmon@sparkpost.com>
2020-12-07 14:03:38 -0500

    main - Change reference

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const GITHUB_API_URL = 'https://api.github.com/graphql'
-const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY
+const GITHUB_REPOSITORY = process.env.GITHUB_REPO
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL
 const SLACK_CHANNEL = process.env.SLACK_CHANNEL || process.env.INPUT_SLACKCHANNEL
